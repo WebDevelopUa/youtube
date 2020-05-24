6d34befa8027455ee2161823c29f460767ac831d
WebDevelopUa <js.develop.com@gmail.com>
2020-05-24 13:26:50 +0000

    Updates

---
+++ precache-manifest.e1ae325d1e07a1c7ccac8d25427cea02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a616d80b13ccab136ecab3acbb87928",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5caaa52873441e2757f5",
+    "url": "./static/css/main.e959fd94.chunk.css"
+  },
+  {
+    "revision": "20cb68d6181d2c8f1229",
+    "url": "./static/js/2.4e4d5d64.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "./static/js/2.4e4d5d64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5caaa52873441e2757f5",
+    "url": "./static/js/main.b1eeaae6.chunk.js"
+  },
+  {
+    "revision": "f60225b81cd3e5f3c45e",
+    "url": "./static/js/runtime-main.e344c7ea.js"
+  }
+]);
